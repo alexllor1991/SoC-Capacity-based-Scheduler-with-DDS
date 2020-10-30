33e43e14827144e42070d8fa890e396b03b97787
alexllor1991 <33665588+alexllor1991@users.noreply.github.com>
2020-10-30 15:09:24 +0100

    Add files via upload

---
+++ My_Scheduler/cluster/setup.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+# set -e
+
+# script creates a Kubernetes cluser from scratch
+# configuration file is cluser-config.yaml
+
+
+# if set to 1, create dashboard
+create_dashboard=1
+# if set to 1 create admin account
+create_admin=1
+# if set to 1 create metrics server deployment
+create_metrics_server=1
+
+# set path to kind
+export PATH=$PATH:$(go env GOPATH)/
+
+if [ "$1" != "" ]; then
+    cluster_name=$1
+else
+    cluster_name="kind"
+fi
+
+echo $cluster_name > CLUSTERNAME
+
+if [ "$2" != "" ]; then
+    echo "Passed to many arguments"
+    echo -e "usage:\n./setup.sh 'cluster name' "
+    exit 1
+fi
+
+kind delete cluster --name=$cluster_name
+kind create cluster --config kind-config.yml --name=$cluster_name
+
+export KUBECONFIG="$(kind get kubeconfig-path --name=$cluster_name)"
+# TODO copy new claster config file to all directories where it is required
+
+if [ $create_dashboard == 1 ]; then
+    echo "Creating dashboard..."
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0/aio/deploy/recommended.yaml
+
+    # we have to wait to assign ip address to this pod TODO do while loop
+    echo "Waiting for dashboard scheduling..."
+    sleep 20
+    # TODO make this working Error from server (NotFound): pods "kubernetes-dashboard-xyz" not found
+    # pod_name=$(kubectl get pods -o wide --all-namespaces | grep "dashboard-" | awk '{printf $2}')
+    # kubectl wait --for=condition=Running pod/$pod_name --all-namespaces
+
+    # change dashboard type to NodePort
+    kubectl patch svc -n kubernetes-dashboard kubernetes-dashboard --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]'
+
+    # address and port of node on which dashboard is running
+    #node_name=$(kubectl get pods -o wide --all-namespaces | grep dashboard- | awk '{print $8}')
+    #tmp_address=$(kubectl get nodes -o wide | grep "$(echo $node_name)" | awk '{print $6}')
+    #tmp_port=$(kubectl -n kubernetes-dashboard get service kubernetes-dashboard  |  awk 'FNR == 2 {print $5}' | cut -d":" -f 2 | cut -d"/" -f 1)
+
+    #echo -e "Dashboard is running at https://$tmp_address:$tmp_port"
+    echo "Dashboard is installed"
+    
+else
+    echo "Dashboard not created"
+fi
+
+if [ $create_admin == 1 ]; then
+    echo "Admin account created"
+    kubectl create -n kube-system serviceaccount admin
+    kubectl create clusterrolebinding permissive-binding \
+     --clusterrole=cluster-admin \
+     --user=admin \
+     --user=kubelet \
+     --group=system:serviceaccounts
+
+     admin_token_name=$(kubectl -n kube-system get serviceaccount admin -o yaml | tail -1 | cut -d":" -f 2 | cut -d" " -f2)
+     admin_token=$(kubectl -n kube-system get secret $admin_token_name -o yaml | grep token | head -1 | sed -e 's/.* \(.*\)$/\1/')
+     echo "Admin token: "
+     echo $admin_token | base64 --decode
+     echo ""
+else
+    echo "Admin account not created"
+fi
+
+# create metrics server-deployment
+if [ $create_metrics_server == 1 ]; then
+    echo "Creating metrics server deployment..."
+    kubectl create -f ../metrics_server_deploy/1.8+/
+    echo "Mertics server would be ready in couple minutes."
+    resp=$(kubectl top nodes)
+    echo "Waiting for metrics server..."
+    #sleep 40
+    # TODO fix this
+    # resp=$(kubectl top nodes)
+    while [ "$resp" = "error: metrics not available yet" ];
+        do
+            resp=$(kubectl top nodes)
+        done
+    echo "Metrics server ready!"
+else
+    echo "Metrics server not created"
+fi
+
+echo "To see the dashboard execute the command kubectl proxy and use the following link http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/"
+echo "To authenticate in the gui use the following Admin token: "
+echo $admin_token | base64 --decode
+echo ""
---
+++ My_Scheduler/pkg/scheduler/src/node.py
@@ -0,0 +1,71 @@
+from kubernetes import client
+
+from pod import PodList
+
+
+class NodeList(object):
+    def __init__(self):
+        self.items = []
+
+
+class Node(object):
+    def __init__(self, metadata_, spec_, status_):
+        """
+        :param V1ObjectMeta metadata_:
+        :param V1NodeSpec spec_:
+        :param V1NodeStatus status_:
+        :return:
+        """
+        if type(metadata_) is not client.models.v1_object_meta.V1ObjectMeta:
+            raise str("Passed invalid type")
+        if type(spec_) is not client.models.V1NodeSpec:
+            raise str("Passed invalid type")
+        if type(status_) is not client.models.V1NodeStatus:
+            raise str("Passed invalid type")
+
+        self.usage = {}
+        self.pods = PodList()
+
+        self.metadata = metadata_
+        self.spec = spec_
+        self.status = status_
+
+    def update_node(self, pod_list):
+        """
+        Update Node Pods and usage attributes
+        :param PodList pod_list: list of Pods
+        :return:
+        """
+        self.pods = self.get_pods_on_node(pod_list)
+        self.usage = self.get_node_usage()
+
+    def get_node_usage(self):
+        """
+        Calculate Node usage based on usage of
+        Pods running on this node
+        :return:
+        """
+        memory = 0
+        cpu = 0
+        for pod in self.pods.items:
+            if pod.is_alive:
+                # there can be pods not collected by garbage collector yet
+                memory += int(pod.get_usage()['memory'])
+                cpu += int(pod.get_usage()['cpu'])
+
+        return {'cpu': cpu, 'memory': memory}
+
+    def get_pods_on_node(self, pod_list):
+        """
+        Browse all available Pods in cluster and
+        assign them to Node
+        :param PodList pod_list: list of Pods
+        :return PodList: return list of Pods running
+            on this Node
+        """
+        result = PodList()
+        for pod in pod_list.items:
+            if pod.spec.node_name == self.metadata.name:
+                result.items.append(pod)
+
+        return result
